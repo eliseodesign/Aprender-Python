e6ef657de45a685a304c689cfb759e05b6a1a6db
Eliseo Francisco Arévalo Espinoza <96401071+eliseodesign@users.noreply.github.com>
2022-09-13 09:47:08 -0600

    Add files via upload

---
+++ Leccion01-04/4_string.py
@@ -0,0 +1,13 @@
+# datos en python tipo string
+
+str1 = "1"
+str2 = "2"
+
+print(int(str1) + int(2))  # 3 ya que int() los convierte a entero,
+# asi como float los convertiria a flotante
+
+
+a = "1"
+b = 1
+
+print(a+str(b)) #daria error el sumar un int y un str
