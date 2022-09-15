3d301a4844a688f70055b086715ed425cddb68f2
Eliseo Francisco Arévalo Espinoza <96401071+eliseodesign@users.noreply.github.com>
2022-09-15 13:38:13 -0600

    Add files via upload

---
+++ Leccion01-04/7_problemaDeSololearn.py
@@ -0,0 +1,8 @@
+# debemos elegir entre un millon de dolares o un centavo que cada dia se duplicara hasta 30 dias
+# debemos calcular en 30 dias cuanto tendremos
+# 1,000,000 o 0.01
+
+millon = 1000000
+numero = 0.01
+
+print(numero*(2**30))
