02ec6564497d9529d0d80452581ac2fff5a1f817
Eliseo Francisco Arévalo Espinoza <96401071+eliseodesign@users.noreply.github.com>
2022-10-22 07:31:47 -0600

    dictionary

---
+++ Leccion06-07/RESUMEN.py
@@ -0,0 +1,9 @@
+#LECCIÓN 06
+# Los ciclos while y for nos sirven para recorrer, repetir iteraciones en elementos o rangos
+#for permite recorer tipo de datos como string, listas, diccionarios, set, y duplas
+
+#LECCION 07
+# tipos de datos que contienen otros datos
+# como lista : que es un conjunto de elementos, ordenados, con indice, y modificable
+# duplas: conjunto de elementos, ordenados, inmodificable,
+# set: conjunto de elementos, sin orden, inmodificable, y no acepta datos repetidos
